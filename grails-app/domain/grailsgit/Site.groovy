10522d6d358b65a49553cf2fa9b9611a089546f1
brunito1 <bruno.bergoglio@gmail.com>
2018-03-05 13:04:06 -0300

    Se modifica el modelo de Site para aceptar los valores de idSite y active

--- grails-app/domain/grailsgit/Site.groovy
+++ grails-app/domain/grailsgit/Site.groovy
@@ -1,9 +1,14 @@
 package grailsgit
 
 class Site {
-
+    String idSite
+    Boolean active
     String name
     static hasMany = [categories:Category]
+
     static constraints = {
+        idSite nullable: false, blank: false
+        active nullable: false, blank: false
+        name nullable: false, blank: false
     }
 }
