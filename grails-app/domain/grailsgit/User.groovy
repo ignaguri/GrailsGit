6d722c1ff115346031f055ec2aaea3ddca4f7a91
ignaguri <ignacioguri@gmail.com>
2018-03-05 12:59:26 -0300

    Esqueleto básico creado

--- grails-app/domain/grailsgit/User.groovy
+++ grails-app/domain/grailsgit/User.groovy
@@ -4,6 +4,12 @@ class User {
 
     String name
     String lastName
+    String user
+    String password
     static constraints = {
+        name nullable: false, blank: false
+        lastName nullable: false, blank: false
+        user nullable: false, blank: false
+        password nullable: false, blank: false
     }
 }
